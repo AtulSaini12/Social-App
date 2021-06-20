977a9a855ddc2f372cc930b456c19b213a40aba3
Atul Saini <atulsaini9886@gmail.com>
2021-06-20 10:45:21 +0530

    Added jwt-decode and retrieved token from local storage and git user data from it

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -60,6 +60,7 @@ export function login(email, password) {
         console.log('data :: ', data);
 
         if (data.success) {
+          localStorage.setItem('token', data.data.token);
           dispatch(loginSuccess(data.data.user));
           return;
         }
--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import * as jwtDecode from 'jwt-decode';
 import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 
 import { fetchPosts } from '../actions/posts';
@@ -8,6 +9,11 @@ import { Home, Navbar, Login } from '.';
 class App extends React.Component {
   componentDidMount() {
     // this.props.dispatch(fetchPosts());
+
+    const token = localStorage.getItem('token');
+    if (token) {
+      const user = jwtDecode(token);
+    }
   }
 
   render() {
